095f0382815dfbfe9fb8a3f5fedac66e6bf0a39d
Giovanni Vazquez <giovanni.vazquez@g-global.com>
2023-02-09 19:34:21 -0800

    feat: Add login

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import Login from './login/login';
 
-export default function Home() {
+import { Grid } from '@material-ui/core';
+import FormLogin from './components/formLogin';
+
+export default function Login() {
   return (
     <Grid
       container
       component="main"
     >
-      <Login />
+      <FormLogin />
     </Grid>
   );
 }
